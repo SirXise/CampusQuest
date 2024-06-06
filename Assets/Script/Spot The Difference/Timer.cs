e7bc9ef3eaf1cd51e7bb14137fe6813f8367c8c7
Awangku Aniq <eatcookiesbeast@gmail.com>
2024-06-06 22:33:18 +0800

    Changed name of timer

--- Assets/Script/Spot The Difference/Timer.cs
+++ Assets/Script/Spot The Difference/Timer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
+public class TimerSpotTheDifference : MonoBehaviour
 {
     public float timeLeft;
     public LogicManager logic;
