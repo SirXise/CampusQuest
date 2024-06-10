2c6f7c6a9f51a9ca4dd9b26a1eb0786e4ffb4367
Nuh Akmal Bin Mohammad <93085446+SirXise@users.noreply.github.com>
2024-06-11 05:54:57 +0800

    Update some things

--- Assets/Script/Essential/CloseTutorial.cs
+++ Assets/Script/Essential/CloseTutorial.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DialogueEditor;
 public class CloseTutorial : MonoBehaviour
 {
+    [SerializeField] private NPCConversation myConversation;
     public GameObject Tutorial;
+
     public void closeTutorial()
     {
         Tutorial.SetActive(false);
+        ConversationManager.Instance.StartConversation(myConversation);
     }
 }
